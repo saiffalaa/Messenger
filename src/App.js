5f3ed7dfb175f3146e4f62e4574796499b344839
Saif-Alaa-ElDin <saifalaa95@gmail.com>
2021-06-05 23:58:55 +0200

    Adding typing message functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import "./App.css";
-
+import React, { useState } from "react";
 function App() {
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([]);
+  console.log(messages);
+  const sendMessage = (e) => {
+    setMessages([...messages, input]);
+    setInput("");
+  };
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <input
+        value={input}
+        placeholder="Enter your message"
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={sendMessage}>Send Message</button>
     </div>
   );
 }
