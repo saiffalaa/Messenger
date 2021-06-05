c360a6f181255710bffb91b5e98c883d8df96cc1
Saif-Alaa-ElDin <saifalaa95@gmail.com>
2021-06-06 00:11:23 +0200

    Added material UI

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import "./App.css";
 import React, { useState } from "react";
+import { Button } from "@material-ui/core";
 function App() {
   const [input, setInput] = useState("");
   const [messages, setMessages] = useState([]);
   console.log(messages);
   const sendMessage = (e) => {
+    e.preventDefault();
     setMessages([...messages, input]);
     setInput("");
   };
   return (
     <div className="App">
-      <input
-        value={input}
-        placeholder="Enter your message"
-        onChange={(e) => setInput(e.target.value)}
-      />
-      <button onClick={sendMessage}>Send Message</button>
+      <form>
+        <input
+          value={input}
+          placeholder="Enter your message"
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button type="submit" onClick={sendMessage}>
+          Send Message
+        </button>
+        {messages.map((message) => {
+          return <p>{message}</p>;
+        })}
+      </form>
     </div>
   );
 }
